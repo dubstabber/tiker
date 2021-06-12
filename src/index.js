72c482eabd9b4c19a37a3c1bc2784769e4e55f90
Dubstabber <taesan@protonmail.com>
2021-06-12 17:25:39 +0200

    Add route pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+
+import './index.css'
+
+function App() {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path='/upload' component={Upload} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App
 
 ReactDOM.render(
   <React.StrictMode>
