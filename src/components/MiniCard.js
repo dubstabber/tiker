d969c490d9c17b111aa080f1aa0dfa32350bc67b
Dubstabber <taesan@protonmail.com>
2021-06-14 15:21:28 +0200

    Add and style follow buttons in suggested accounts

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function MiniCard() {
+function MiniCard({user}) {
     return (
-        <div>
-            
+        <div className="section minicard">
+            <div className="section">
+                    <img className="user-profile" src={user.avatar} width={'100%'} />
+                <div>
+                    <h3 className="bold">{user.username}</h3>
+                    <p>{user.name}</p>
+                </div>
+            </div>
+            {user.button_visible && <div className={user.is_followed ? "followed-button" : "follow-button"}>
+              {user.is_followed ? "Following" : "Follow"}
+            </div>}
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Card from '../components/Card'
+import MiniCard from '../components/MiniCard'
 import FollowersColumn from '../components/FollowersColumn'
 import axios from 'axios'
 
@@ -7,6 +8,7 @@ function Home() {
     const [users, setUsers] = useState(null)
     let descendingUsers
     let topFiveFollowing
+    let topFiveNotFollowing
 
 
     const fetchData = async () => {
@@ -24,9 +26,12 @@ function Home() {
         const following = users.filter(user => user.is_followed)
         const descendingFollowing = following.sort((a,b) => a.likes < b.likes ? 1 : -1)
         topFiveFollowing = descendingFollowing.slice(0,5)
+
+        const notFollowing = users.filter(user => user.is_followed === false)
+        const descendingNotFollowing = notFollowing.sort((a,b) => a.likes < b.likes ? 1 : -1)
+        topFiveNotFollowing = descendingNotFollowing.slice(0,5)
     }
 
-    console.log(descendingUsers)
 
     return (
         <>
@@ -47,6 +52,12 @@ function Home() {
                         <div className='suggested'>
                             <h2 className='bold'>Suggested accounts</h2>
                             <div className='break' />
+                                {topFiveNotFollowing.map((notFollowingUser, index) => (
+                                    <MiniCard 
+                                        key={index}
+                                        user={notFollowingUser}
+                                    />
+                                ))}
 
                         </div>
                     </div>
