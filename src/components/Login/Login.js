7112bda82b54405b83af900916d8c81385f2ec31
Dubstabber <taesan@protonmail.com>
2021-06-17 18:42:55 +0200

    Style up the modal login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,7 @@ import {AppContext} from '../../context'
 import './Login.styles.css'
 
 const Login = () => {
-    const {showLogin, setShowLogin} = useContext(AppContext);
+    const {showLogin, setShowLogin} = useContext(AppContext)
 
     const disableLogin = () => {
         setShowLogin(false)
@@ -11,14 +11,25 @@ const Login = () => {
 
     return (
         <>
-            {showLogin && <>
+            {showLogin &&<>
                 <div onClick={disableLogin} className='modal-box'></div>
                 <div className='box'>
-                    <form className="login-form">
-                        <div className='login-title'>Log in to Tiker</div>
-                        <input type='email' className='email-input'/>
-                        <input type='password' className='password-input'/>
-                    </form>
+                    <div className="login-container">
+                        <form className="login-form">
+                            <div className='login-title'>Log in to Tiker</div>
+                            <div className='email-field'>
+                                <input placeholder="Email" type='email' className='email-input'/>
+                            </div>
+                            <div className='email-field'>
+                                <input placeholder="Password" type='password' className='password-input'/>
+                            </div>
+                            <button className="submit-login">Log in</button>
+                        </form>
+                    </div>
+                    <hr />
+                    <div className='register-proposal'>
+                        <span>Don't have an account?</span><span className="register-link">Sign up</span>
+                    </div>
                 </div>
             </>}
         </>
