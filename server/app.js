d469f73c3d5f1084490f164353c4a51218232c96
Dubstabber <taesan@protonmail.com>
2021-06-19 17:16:06 +0200

    Add registration function

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const { body, validationResult } = require('express-validator');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
-mongoose.connect('mongodb://localhost/tikerDB', {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
+mongoose.connect('mongodb://localhost/tikerDB', {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('connected to mongoDB!')
 });
@@ -23,8 +24,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     avatar: String,
     following: Array,
-    followers: Array
-});
+    followers: Array,
+    createdAt: String
+})
 
 const postSchema = new mongoose.Schema({
     id: String,
@@ -33,25 +35,65 @@ const postSchema = new mongoose.Schema({
     likes: Number,
     comments: Number,
     timestamp: String
-});
+})
 
-const User = mongoose.model('User', userSchema);
-const Post = mongoose.model('Post', postSchema);
+const User = mongoose.model('User', userSchema)
+const Post = mongoose.model('Post', postSchema)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.json({
         status: 'server works!'
-    });
-});
+    })
+})
+
+app.post('/register', 
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 }),
+    body('password2').isLength({ min: 6 }),
+    (req, res) => {
+        if(req.body.password !== req.body.password2){
+            return res.send('Passwords do not match')
+        }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        User.find({email: req.body.email}, (err, arr) => {
+            if(!err){
+                if(arr.length !== 0){
+                    return res.send(`account ${req.body.email} exists`)
+                }
+            }
+        })
+        const defaultUsername = req.body.email.slice(0,req.body.email.indexOf('@'))
+        const newUser = {
+            name: "",
+            username: defaultUsername,
+            password: req.body.password,
+            email:req.body.email,
+            avatar: null,
+            following: [],
+            followers: [],
+            createdAt: new Date()
+        }
+        User.create(newUser, (err, arr) => {
+            if(err){
+                console.log(`Account could not be created: ${err}`)
+                return res.send("Account could not be created")
+            } else {
+                return res.send(`Account '${req.body.email}' has been created`)
+            }
+        })
+})
 
 app.get('/posts', (req, res) => {
     User.find(null, (err, arr)=>{
         res.json(arr);
     })
-});
+})
 
 app.put('/edit/:id', (req, res) => {
     const data = JSON.parse(req.query.data)
@@ -59,7 +101,7 @@ app.put('/edit/:id', (req, res) => {
     if(data){
         User.updateOne({_id: req.params.id}, data, (err, arr) => {
             res.json(arr);
-        });
+        })
     }
 })
 
@@ -67,10 +109,10 @@ app.post('/add', (req, res) => {
     if(req.body){
         User.create(req.body).then(data => {
             res.send('OK')
-        });
+        })
     }
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
--- src/components/ModalDialog/RegisterDialog/RegisterDialog.js
+++ src/components/ModalDialog/RegisterDialog/RegisterDialog.js
@@ -1,21 +1,35 @@
+import axios from 'axios'
 import React from 'react'
 
 import './RegisterDialog.styles.css'
 
 const RegisterDialog = ({switchPage}) => {
+
+    const registerUser = (e) => {
+        e.preventDefault()
+        const registerData = {
+            email: e.target.email.value,
+            password: e.target.password.value,
+            password2: e.target.password2.value
+        }
+        axios.post('http://localhost:5000/register', registerData).then(res => {
+            console.log(res.data)
+        })
+    }
+
     return (
         <>
             <div className="register-container">
-                <form className="register-form">
+                <form onSubmit={registerUser} className="register-form">
                     <div className='register-title'>Sign up for Tiker</div>
                     <div className='email-field'>
-                        <input placeholder="Email" type='email' className='email-input'/>
+                        <input placeholder="Email" type='email' className='email-input' name='email'/>
                     </div>
                     <div className='password-field'>
-                        <input placeholder="Password" type='password' className='password-input'/>
+                        <input placeholder="Password" type='password' className='password-input' name='password'/>
                     </div>
                     <div className='password-field'>
-                        <input placeholder="Repeat password" type='password' className='password-input'/>
+                        <input placeholder="Repeat password" type='password' className='password-input' name='password2'/>
                     </div>
                     <button className="submit-register">Sign up</button>
                 </form>
