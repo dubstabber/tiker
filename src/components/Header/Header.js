595b69b113982660e5adb89e63a78026caec8102
Dubstabber <taesan@protonmail.com>
2021-06-27 12:52:44 +0200

    Add tooltip and implement logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,23 @@
 import React, {useContext} from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {AppContext} from '../../context'
 import './Header.styles.css'
 
-const Header = () => {
+const Header = (props) => {
   const {user, setShowModalDialog} = useContext(AppContext)
 
   const handleLogin = () => {
     setShowModalDialog(true);
   }
 
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    window.location.reload()
+  }
+
   return (
     <div className="header">
-      <Link to='/'>
+      <Link to='/' onClick={() => window.location.reload()}>
         <div className="logo"></div>
       </Link>
       <div className="upload-container">
@@ -20,7 +25,16 @@ const Header = () => {
           <Link to='/upload'>
             <div className="upload" />
           </Link>
-          {user.isAuth ? <img className="personal" src={user.avatar ? user.avatar : './images/user-icon.jpg'} alt='personal'/>
+          {user.isAuth ? 
+            <div>
+              <img className="personal" src={user.avatar ? user.avatar : './images/user-icon.jpg'} alt='personal'/>
+              <div className='personal-options'>
+                <p className="personal-option">View profile</p>
+                <p className="personal-option">Settings</p>
+                <hr/>
+                <p onClick={handleLogout} className="personal-option">Log out</p>
+              </div>
+            </div>
         : <button onClick={handleLogin} className='login-btn'>Login</button>  
         }
         </div>
